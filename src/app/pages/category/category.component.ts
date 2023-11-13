87cb54c9c912591d12b6ea77ccd18e20e310c975
Bao Ngoc Ta <bta@myseneca.ca>
2023-11-12 23:13:48 -0500

    main UI for category page

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -3,8 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent {
+  categories: { category: string; cate_img: string }[] = [];
+  posts: number[] = [12, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13];
 
+  ngOnInit(): void {
+    this.categories = [
+      {
+        category: 'category',
+        cate_img: 'https://picsum.photos/id/944/900/500',
+      },
+      { category: 'Samoa', cate_img: 'https://picsum.photos/id/1011/900/500' },
+      { category: 'Tonga', cate_img: 'https://picsum.photos/id/984/900/500' },
+      {
+        category: 'Cook Island',
+        cate_img: 'https://picsum.photos/id/944/900/500',
+      },
+      { category: 'Niue', cate_img: 'https://picsum.photos/id/1011/900/500' },
+      {
+        category: 'American Samoa',
+        cate_img: 'https://picsum.photos/id/984/900/500',
+      },
+      {
+        category: 'category',
+        cate_img: 'https://picsum.photos/id/944/900/500',
+      },
+      { category: 'Samoa', cate_img: 'https://picsum.photos/id/1011/900/500' },
+      { category: 'Tonga', cate_img: 'https://picsum.photos/id/984/900/500' },
+      {
+        category: 'Cook Island',
+        cate_img: 'https://picsum.photos/id/944/900/500',
+      },
+      { category: 'Niue', cate_img: 'https://picsum.photos/id/1011/900/500' },
+      {
+        category: 'American Samoa',
+        cate_img: 'https://picsum.photos/id/984/900/500',
+      },
+      {
+        category: 'American Samoa',
+        cate_img: 'https://picsum.photos/id/984/900/500',
+      },
+    ];
+  }
 }
